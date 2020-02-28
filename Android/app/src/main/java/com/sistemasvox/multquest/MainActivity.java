806d684b43bc48283b121649ea434b74c742a01e
Marcelo <dj.marcelo.2009@gmail.com>
2020-02-28 13:50:44 -0300

    menu gaveta 0%

--- Android/app/src/main/java/com/sistemasvox/multquest/MainActivity.java
+++ Android/app/src/main/java/com/sistemasvox/multquest/MainActivity.java
@@ -1,51 +1,69 @@
 package com.sistemasvox.multquest;
 
 import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-import com.sistemasvox.multquest.dao.AlternativaDAO;
-import com.sistemasvox.multquest.dao.AreaConhecimentoDAO;
-import com.sistemasvox.multquest.dao.ConteudoDAO;
-import com.sistemasvox.multquest.dao.DisciplinaDAO;
-import com.sistemasvox.multquest.dao.QuestaoDAO;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //atualizarTotal();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_f_simulado, R.id.nav_r_questoes,
+                R.id.nav_s_realizados, R.id.nav_p_aprendizagem, R.id.nav_sobre)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
-    private void atualizarTotal() {
-        TextView questoes = findViewById(R.id.txtQ);
-        TextView alternativas = findViewById(R.id.txtA);
-        TextView areConhe = findViewById(R.id.txtAC);
-        TextView conteudo = findViewById(R.id.txtC);
-        TextView disc = findViewById(R.id.txtD);
-
-        try {
-            questoes.setText(new QuestaoDAO(this).getTotalQuestoes());
-        } catch (Exception e) {
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
-        }
-        alternativas.setText(new AlternativaDAO(this).getTotalAlternativas());
-        areConhe.setText(String.valueOf(new AreaConhecimentoDAO(this).getAlternativas().size()));
-        conteudo.setText(new ConteudoDAO(this).getTotalConteudo());
-        disc.setText(new DisciplinaDAO(this).getAllDisciplinas().size() + "");
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
-    public void botaoDados(View v) {
-        setContentView(R.layout.activity_main2);
-        atualizarTotal();
-    }
-
-    public void botaoMenu(View v) {
-        setContentView(R.layout.home);
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
